30f00367027ef1be84577aa97783440fb4a63b2a
Kannan Raveendran Nair <kannan4355@gmail.com>
2012-08-21 22:35:08 +0530

    Modifcation to use variable
    Reads input from console

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,25 @@
+import java.io.*;
+
 public class HelloWorld{
-public static void main(String[] args){
-	System.out.println("Hello World");
+		
+	public static void main(String[] args){
+		String name = null;
+		
+		//Print message to screen
+		System.out.println("Enter name : ");
+		
+		//Get input from user
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		try{
+			name = br.readLine();
+		}
+		catch(IOException e){
+			System.out.println("Error in reading");
+			System.exit(1);
+		}
+		
+		//Print back the name
+		System.out.println("Hello " + name);
 	}
 }
